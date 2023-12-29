c3c150cd334bb9b99f53dc4177172548fe0d3db6
Evan <suzukaze.hazuki2020@gmail.com>
2023-12-29 21:19:04 +0900

    nike

---
+++ Nike/Nike/NikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NikeApp.swift
+//  Nike
+//
+//  Created by Balaji on 12/07/20.
+//
+
+import SwiftUI
+
+@main
+struct NikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
