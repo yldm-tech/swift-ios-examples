4bb4456f9a0c9ddd54a6f9b72cab149d6003622b
Evan <suzukaze.hazuki2020@gmail.com>
2023-12-29 21:37:40 +0900

    Solid

---
+++ Solid/Solid/SolidApp.swift
@@ -0,0 +1,18 @@
+//
+//  SolidApp.swift
+//  Solid
+//
+//  Created by Balaji on 08/07/20.
+//
+
+import SwiftUI
+
+@main
+
+struct SolidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
