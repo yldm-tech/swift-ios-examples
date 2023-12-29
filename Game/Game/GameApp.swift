9009169d772638a4a3b68a9056101136e2f068ec
Evan <suzukaze.hazuki2020@gmail.com>
2023-12-29 21:26:44 +0900

    Game

---
+++ Game/Game/GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameApp.swift
+//  Game
+//
+//  Created by Balaji on 26/06/20.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
