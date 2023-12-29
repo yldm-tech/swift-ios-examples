d773bbbfee880a8cb18e57339d0580ed365645ce
Evan <suzukaze.hazuki2020@gmail.com>
2023-12-29 21:36:10 +0900

    Parallax

---
+++ Parallax/Parallax/ParallaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxApp.swift
+//  Parallax
+//
+//  Created by Balaji on 06/07/20.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
