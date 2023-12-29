4cb392e5c55f7d88334990b6ea81e144d12cd06c
Evan <suzukaze.hazuki2020@gmail.com>
2023-12-29 21:39:15 +0900

    Neuro

---
+++ Neuro/Neuro/NeuroApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuroApp.swift
+//  Neuro
+//
+//  Created by Balaji on 16/07/20.
+//
+
+import SwiftUI
+
+@main
+struct NeuroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
