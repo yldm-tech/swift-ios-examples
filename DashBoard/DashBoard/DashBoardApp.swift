8a53800a665a894ce854cb9da28c67f5d52e7275
Evan <suzukaze.hazuki2020@gmail.com>
2023-12-29 21:23:32 +0900

    dashboard

---
+++ DashBoard/DashBoard/DashBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DashBoardApp.swift
+//  DashBoard
+//
+//  Created by Balaji on 29/06/20.
+//
+
+import SwiftUI
+
+@main
+struct DashBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
